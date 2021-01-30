f240a51646534dff39ebd862de7fcaf9d26ca43e
SomeOne <SomeOne@DESKTOP-9L1ADB9>
2021-01-31 01:08:14 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,28 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //iş kodları buraya yazılır.
+            //yetkisi var mı vs... if ler...
+
+            return _productDal.GetAll();
+        }
+    }
+}
